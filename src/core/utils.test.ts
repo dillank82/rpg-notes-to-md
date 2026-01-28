8a1f845cdac0c8d146339b5d108e5153dd9b2e61
dillank82 <dillankyuppi@gmail.com>
2026-01-28 12:26:34 +0500

    add tests to sanitizeName

--- src/core/utils.test.ts
+++ src/core/utils.test.ts
@@ -1,4 +1,4 @@
-import { normalizePath } from "./utils"
+import { normalizePath, sanitizeName } from "./utils"
 
 describe('normalizePath', () => {
     it('should replace backslashes with forward slashes; collapse multiple slashes; remove slash at the beginning; remove slash at the end if it is file path; trim whitespaces', () => {
@@ -15,4 +15,23 @@ describe('normalizePath', () => {
         const secondRun = normalizePath(firstRun)
         expect(firstRun).toBe(secondRun)
     })
-}) 
+})
+
+describe('sanitizeName', () => {
+    it('should keep valid names', () => {
+        const validName = 'Normal Name of Some-Char_01'
+        expect(sanitizeName(validName)).toBe(validName)
+    })
+    it('should trim the spaces at the edges', () => {
+        expect(sanitizeName('   name        ')).toBe('name')
+    })
+    it('should remove dots and spaces only at the end', () => {
+        expect(sanitizeName('t.e.x.t with whitespaces ...  . .   .. . ')).toBe('t.e.x.t with whitespaces')
+    })
+    it('should replace prohibited characters with a hyphen', () => {
+        expect(sanitizeName('\\1/2:3*4?5"6<7>8|9#10^11[12]13%14')).toBe('-1-2-3-4-5-6-7-8-9-10-11-12-13-14')
+    })
+    it('should return "Untitled" if the result is empty', () => {
+        expect(sanitizeName('.. . . .    .')).toBe('Untitled')
+    })
+})
