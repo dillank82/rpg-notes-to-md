4fd4a5c72a8d8fe8b1d04b14d57c24bad8c2b7de
dillank82 <dillankyuppi@gmail.com>
2026-01-30 12:03:37 +0500

    add tests for pathRegistry

---
+++ src/core/vfs/pathRegistry.test.ts
@@ -0,0 +1,44 @@
+import { PathRegistry } from "./pathRegistry"
+
+describe('pathRegistry', () => {
+    it('should return valid path unchanged if registry is empty', () => {
+        const registry = new PathRegistry()
+        expect(registry.resolve('path')).toBe('path')
+    })
+    it('should return a sequence of different valid paths unchanged', () => {
+        const registry = new PathRegistry()
+        const path1 = registry.resolve('a')
+        const path2 = registry.resolve('b')
+        expect(path1).toBe('a')
+        expect(path2).toBe('b')
+    })
+    it('should resolve a single collision', () => {
+        const registry = new PathRegistry()
+        const path1 = registry.resolve('note.md')
+        const path2 = registry.resolve('note.md')
+        expect(path1).toBe('note.md')
+        expect(path2).toBe('note-1.md')
+    })
+    it('should resolve a multiple collision', () => {
+        const registry = new PathRegistry()
+        registry.resolve('note.md')
+        registry.resolve('note.md')
+        expect(registry.resolve('note.md')).toBe('note-2.md')
+    })
+    it('should not duplicate occupied suffixes when resolving collisions', () => {
+        const registry = new PathRegistry()
+        registry.resolve('note.md')
+        registry.resolve('note-1.md')
+        expect(registry.resolve('note.md')).toBe('note-2.md')
+    })
+    it('should compare paths case-insensitively', () => {
+        const registry = new PathRegistry()
+        registry.resolve('note.md')
+        expect(registry.resolve('NoTE.md')).toBe('NoTE-1.md')
+    })
+    it('should work correctly with .md in the file name', () => {
+        const registry = new PathRegistry()
+        registry.resolve('note.md.md')
+        expect(registry.resolve('note.md.md')).toBe('note.md-1.md')
+    })
+})
