6aff142f6969ec44d9f4402c6c981d5445f46a44
dillank82 <dillankyuppi@gmail.com>
2026-01-30 17:16:29 +0500

    add tests for VFSBuilder; document known bugs in comments (marked as skipped)

---
+++ src/core/vfs/VFSBuilder.test.ts
@@ -0,0 +1,164 @@
+import { CampaignsData, RPGNotesDataMaps } from "../../interfaces/RPGNotesData"
+import { buildStructure } from "./VFSBuilder"
+
+describe('VFSBuilder', () => {
+    const createMockData = (overrides = {}): CampaignsData => ({
+        campaigns: [],
+        categories: [],
+        connections: [],
+        storyNotes: [],
+        subjectNotes: [],
+        subjects: [],
+        subjectTagsAttachments: [],
+        subjectTags: [],
+        ...overrides
+    })
+    const createMockMaps = (overrides = {}): RPGNotesDataMaps => ({
+        categoriesByParentId: new Map<unknown, CampaignsData['categories']>(),
+        subjectsByCategory: new Map<unknown, CampaignsData['subjects']>(),
+        notesBySubject: new Map<unknown, CampaignsData['subjectNotes']>(),
+        tagsAttachmentsBySubject: new Map<unknown, CampaignsData['subjectTagsAttachments']>(),
+        storyNotesByCampaign: new Map<unknown, CampaignsData['storyNotes']>(),
+        ...overrides
+    })
+
+    it.skip('should correctly create file structure', () => {
+        // marked with skip because of problems with generateNoteContent
+        const data = createMockData({
+            campaigns: [{ id: 47, description: ':)', name: 'Cool Campaign' }],
+        })
+        const maps = createMockMaps({
+            categoriesByParentId: new Map([
+                [-1, [{ campaign_id: 47, parentCategory_id: -1, description: 'It should be inside campaign', id: 1, name: 'Root Category' }]],
+                [1, [{ campaign_id: 47, parentCategory_id: 1, description: 'It should be inside Root Category', id: 2, name: 'Sub Category' }]]
+            ]),
+            subjectsByCategory: new Map([
+                [1, [{ category_id: 1, fullDescription: 'Why he is inside Root Category', description: '???', id: 777, name: 'Mr. Mistery' }]],
+                [2, [{ category_id: 2, fullDescription: 'The real hero of the Sub Category', description: 'His might is infinity', id: 8, name: 'Chuck' }]]
+            ]),
+            storyNotesByCampaign: new Map([[47, [{ campaign_id: 47, description: 'Single Story Note, but named as 100', id: 100 }]]])
+        })
+        const vfs = buildStructure(data, maps)
+        expect(vfs).toMatchSnapshot()
+    })
+    it('should return empty object when gets empty data', () => {
+        const data = createMockData()
+        const maps = createMockMaps()
+        const vfs = buildStructure(data, maps)
+        expect(vfs).toEqual({})
+    })
+    it.skip('should correctly integrate with PathRegistry', () => {
+        // bug in integration
+        const data = createMockData({
+            campaigns: [{ id: 1, name: 'campaign' }]
+        })
+        const maps = createMockMaps({
+            categoriesByParentId: new Map([[-1, [{ campaign_id: 1, parentCategory_id: -1, id: 1, name: 'category' }]]]),
+            subjectsByCategory: new Map([
+                [1, [{ category_id: 1, fullDescription: '', id: 1, name: 'subject' }]],
+                [1, [{ category_id: 1, fullDescription: '', id: 2, name: 'subject' }]]
+            ])
+        })
+        const vfs = buildStructure(data, maps)
+        expect('campaign/category/subject.md' in vfs).toBe(true)
+        expect('campaign/category/subject-1.md' in vfs).toBe(true)
+    })
+    it('should correctly add subject notes to file', () => {
+        const data = createMockData({
+            campaigns: [{ id: 1, name: 'campaign' }]
+        })
+        const maps = createMockMaps({
+            categoriesByParentId: new Map([[-1, [{ campaign_id: 1, parentCategory_id: -1, id: 1, name: 'category' }]]]),
+            subjectsByCategory: new Map([[1, [{ category_id: 1, fullDescription: '', id: 1, name: 'subject' }]]]),
+            notesBySubject: new Map([[1, [
+                { subject_id: 1, name: 'subject_note_1' },
+                { subject_id: 1, name: 'subject note 2 (without snake_case)' }
+            ]]])
+        })
+        const vfs = buildStructure(data, maps)
+        const subject = vfs['campaign/category/subject.md']
+        expect(subject).toMatchInlineSnapshot(`
+          "
+                  ---
+                  tags: []
+                  ---
+                  undefined
+                  
+          # Description
+
+                  
+          > subject_note_1
+          > subject note 2 (without snake_case)
+                  
+          #Connections
+              "
+        `)
+        // now it's bad result because of many tabs. should fix and test generateNoteContent and update snapshots then
+    })
+    it.skip('should correctly add tags to file and add name of campaign to non-global tags', () => {
+        const data = createMockData({
+            campaigns: [{ id: 1, name: 'campaign' }],
+            subjectTags: [
+                { id: 1, isGlobal: true, name: 'Global tag' },
+                { id: 2, isGlobal: false, name: 'Local tag' }
+            ]
+        })
+        const maps = createMockMaps({
+            categoriesByParentId: new Map([[-1, [{ campaign_id: 1, parentCategory_id: -1, id: 1, name: 'category' }]]]),
+            subjectsByCategory: new Map([[1, [{ category_id: 1, fullDescription: '', id: 1, name: 'subject' }]]]),
+            tagsAttachmentsBySubject: new Map([[1, [{ subject_id: 1, tag_id: 1 }, { subject_id: 1, tag_id: 2 }]]])
+        })
+        const vfs = buildStructure(data, maps)
+        const subject = vfs['campaign/category/subject.md']
+        expect(subject.startsWith('---\ntags: [Global tag, Local tag (campaign)]\n---')).toBe(true)
+    })
+    it.skip('should correctly add connections to files', () => {
+        const data = createMockData({
+            campaigns: [{ id: 1, name: 'campaign' }],
+            connections: [{ comment_1: 'link description', comment_2: undefined, subject1_id: 1, subject2_id: 2 }]
+        })
+        const maps = createMockMaps({
+            categoriesByParentId: new Map([[-1, [{ campaign_id: 1, parentCategory_id: -1, id: 1, name: 'category' }]]]),
+            subjectsByCategory: new Map([[1, [
+                { category_id: 1, fullDescription: '', id: 1, name: 'subject1' },
+                { category_id: 1, fullDescription: '', id: 2, name: 'subject2' }
+            ]]]),
+        })
+        const vfs = buildStructure(data, maps)
+        const subject1 = vfs['campaign/category/subject1.md']
+        const subject2 = vfs['campaign/category/subject2.md']
+        expect(subject1).toMatchInlineSnapshot(`
+          "
+                  ---
+                  tags: []
+                  ---
+                  undefined
+                  
+          # Description
+
+                  
+
+                  
+          #Connections
+              [[subject2]] - link description 
+          "
+        `)
+        expect(subject2).toMatchInlineSnapshot(`
+          "
+                  ---
+                  tags: []
+                  ---
+                  undefined
+                  
+          # Description
+
+                  
+
+                  
+          #Connections
+              [[subject1]] undefined 
+          "
+        `)
+    })
+    // it's also fake result. should fix these undefined in text
+})
