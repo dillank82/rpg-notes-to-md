91f85e33f9cb47d048e1a593813ebfcaf8f3f7e9
dillank82 <dillankyuppi@gmail.com>
2026-02-02 14:22:08 +0500

    init App component with basic styles

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,9 @@
 export const App = () => {
   return (
-    <>
-    
-    </>
+    <div className="min-h-screen bg-slate-50 flex flex-col items-center">
+      <main className="w-full max-w-2xl bg-white rounded-xl shadow-sm p-6">
+
+      </main>
+    </div>
   )
 }
