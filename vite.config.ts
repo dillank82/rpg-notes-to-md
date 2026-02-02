b941818aa0daf5651da7b231afd5799c90c46c2c
dillank82 <dillankyuppi@gmail.com>
2026-02-02 11:57:20 +0500

    install and configure tailwind css

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
   test: {
     globals: true,
     environment: 'happy-dom',
