3efc42d20f2041030cbfd41e4d9610db09962746
dillank82 <dillankyuppi@gmail.com>
2026-02-04 16:37:04 +0500

    feat: add styles to Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,7 +7,12 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 export const Button = ({ children, tailwind, isLoading, ...props }: ButtonProps) => {
     return(
         <button 
-            className={tailwind}
+            className={
+                'bg-blue-500 text-white py-2.5 px-5 m-2 rounded-lg'
+                +' '+'disabled:bg-gray-400 disabled:cursor-not-allowed'
+                +' '+'focus:outline-solid focus:outline-black focus:outline-2'
+                +' '+tailwind
+            }
             disabled={isLoading}
             {...props}
         >
