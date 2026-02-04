444ab24ec93bfb1d9accd74cff8325dfe5547dbd
dillank82 <dillankyuppi@gmail.com>
2026-02-04 11:03:07 +0500

    style: add fixed height to App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,7 +2,7 @@ import { Converter } from "./Converter"
 
 export const App = () => {
   return (
-    <div className="min-h-screen bg-slate-50 flex flex-col items-center">
+    <div className="h-screen bg-slate-50 flex flex-col items-center">
       <Converter />
     </div>
   )
