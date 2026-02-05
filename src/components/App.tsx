6c2461dfebe50015602f638b4eaf6ee2dd6d50e2
dillank82 <dillankyuppi@gmail.com>
2026-02-05 16:08:49 +0500

    feat: make app text semibold

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,7 +2,7 @@ import { Converter } from "./Converter"
 
 export const App = () => {
   return (
-    <div className="h-screen bg-slate-50 flex flex-col items-center">
+    <div className="h-screen bg-slate-50 flex flex-col items-center font-semibold">
       <Converter />
     </div>
   )
