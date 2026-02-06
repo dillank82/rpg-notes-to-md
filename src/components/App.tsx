05c8657cbbe08fcca80b384914f4da97a2e90418
dillank82 <dillankyuppi@gmail.com>
2026-02-06 11:01:29 +0500

    feat: add bigger font size for widescreen (2xl)

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,6 +5,7 @@ export const App = () => {
     <div className="
       h-screen bg-slate-50 flex flex-col items-center font-semibold
       lg:justify-center lg:pb-[12.5%]
+      2xl:text-lg
     ">
       <Converter />
     </div>
