7e39dc6d4bbe00a30ac2fd362e19753a6b571607
dillank82 <dillankyuppi@gmail.com>
2026-02-09 12:06:50 +0500

    feat: change Button color to violet

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,7 +8,7 @@ type ButtonProps =
     | (BaseProps & AnchorHTMLAttributes<HTMLAnchorElement> & { as: 'a' })
 
 export const Button = (props: ButtonProps) => {
-    const baseStyles = 'bg-blue-500 text-white py-2.5 px-5 m-2 rounded-lg disabled:bg-gray-400 disabled:cursor-not-allowed focus:outline-solid focus:outline-black focus:outline-2'
+    const baseStyles = 'bg-violet-500 text-white py-2.5 px-5 m-2 rounded-lg disabled:bg-gray-400 disabled:cursor-not-allowed focus:outline-solid focus:outline-black focus:outline-2'
     if (props.as === 'a') {
         const { children, tailwind, ...rest } = props
         return (
@@ -27,5 +27,6 @@ export const Button = (props: ButtonProps) => {
         >
             {children}
         </button>
+
     )
 }
