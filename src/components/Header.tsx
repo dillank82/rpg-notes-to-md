d9f47e7fb8d225fa96eca51c5b2440e4eaa0d233
dillank82 <dillankyuppi@gmail.com>
2026-02-16 10:47:17 +0500

    fix: resolve broken logo path by const definition

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,8 @@
 export const Header = () => {
+    const logo = 'src/img/logo.png'
     return (
         <header className="my-3">
-            <img src="src/img/logo.png" alt="RPG Notes to Obsidian.md" className="w-50" />
+            <img src={logo} alt="RPG Notes to Obsidian.md" className="w-50" />
         </header>
     )
 }
