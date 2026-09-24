49631997de30ce538ae2cc358e2b2afb121aadc4
Evgeny <dillank82@gmail.com>
2026-09-24 10:06:33 +0500

    fix(layout): remove overflow-y: scroll from App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,7 +5,7 @@ import { LanguagesSelectWrapper } from "./LanguagesSelectWrapper"
 export const App = () => {
   return (
     <div className="
-      min-h-screen bg-white flex flex-col items-center justify-center font-semibold overflow-y-scroll
+      min-h-screen bg-white flex flex-col items-center justify-center font-semibold
       md:bg-slate-50 md:pb-[10%]
       2xl:text-lg
     ">
