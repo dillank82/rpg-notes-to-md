8eef677219a0d16c866f1efb626af3e4e01ed6d6
Evgeny <dillank82@gmail.com>
2026-09-24 10:53:56 +0500

    fix(layout): replace padding bottom style in App with 2 flex-grow spacers

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,12 +6,17 @@ export const App = () => {
   return (
     <div className="
       min-h-screen bg-white flex flex-col items-center justify-center font-semibold
-      md:bg-slate-50 md:pb-[10%]
+      md:bg-slate-50
       2xl:text-lg
     ">
+      {/* two flex-grow spacers for optical centering on md+ screens */}
+      <div className="flex-1 md:flex-[1]" />
+
       <LanguagesSelectWrapper />
       <Header />
       <Converter />
+
+      <div className="flex-1 md:flex-[2.1]" />
     </div>
   )
 }
