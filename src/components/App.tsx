d343d1832f22f32a2c8acd01723e05594e851c1d
Evgeny Savelyev <dillank82@gmail.com>
2026-09-24 09:46:40 +0500

    fix(App): remove padding bottom style

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,7 +6,7 @@ export const App = () => {
   return (
     <div className="
       min-h-screen bg-white flex flex-col items-center justify-center font-semibold
-      md:bg-slate-50 md:pb-[10%]
+      md:bg-slate-50
       2xl:text-lg
     ">
       <LanguagesSelectWrapper />
